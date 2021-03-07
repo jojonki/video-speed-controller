9814db3dc0dd381ec32fd7e0c75f51af6d3efa5a
jojonki <junki.ohmura@gmail.com>
2021-03-07 13:14:21 +0900

    surppot commands

--- background.js
+++ background.js
@@ -1,44 +1,36 @@
 'use strict';
 
 
-function changeSpeed(tab, speed) {
-    console.log("called changeSpeed");
+function changeSpeed(tab, speed_cmd) {
+    // console.log("called changeSpeed " + speed_cmd);
     let url = tab.url;
     chrome.tabs.executeScript(tab.id, {
-        code: 'var speed=' + speed
+        code: 'var speed_cmd="' + speed_cmd + '"'
     }, function () {
-        // chrome.tabs.executeScript(tab.id, { file: 'inject.js' });
-        chrome.tabs.executeScript(tab.id, { code: "document.querySelector('video').playbackRate=2" });
-
-        let options = {
-            type: "basic",
-            iconUrl: "./images/icon_128.png",
-            title: "Change Speed",
-            message: "\"" + speed + "\"",
-        }
-        console.log("call notifications");
-        chrome.notifications.create('change_speed', options);
+        chrome.tabs.executeScript(tab.id, { file: 'inject.js' },
+            function () {
+                let options = {
+                    type: "basic",
+                    iconUrl: "./images/icon_128.png",
+                    title: speed_cmd,
+                    message: "\"" + "test" + "\"",
+                }
+                console.log("call notifications");
+                chrome.notifications.create('change_speed', options);
+            });
     });
 
 }
 
 chrome.commands.onCommand.addListener(function (command) {
     console.log("onCommand:", command);
-    let copy_mode = null;
-    if (command === "speed-up") {
-        copy_mode = "plain"
-    } else {
-        alert("Unknown command: " + command);
-    }
 
-    console.log("command", copy_mode);
-    if (copy_mode !== undefined) {
-        console.log("call tabs.query to activeTab");
+    if (command !== undefined) {
         chrome.tabs.query({
             active: true,
             lastFocusedWindow: true
         }, function (tabs) {
-            changeSpeed(tabs[0], 2.5);
+            changeSpeed(tabs[0], command);
         });
     }
 });
--- inject.js
+++ inject.js
@@ -1,3 +1,18 @@
 (function () {
-    document.querySelector('video').playbackRate = config;
+    var $video = document.querySelector('video');
+    if ($video !== null) {
+        var speed = $video.playbackRate;
+        if (speed_cmd === "speed-up") {
+            speed += 0.25;
+        } else if (speed_cmd == 'speed-down') {
+            speed -= 0.25;
+        } else if (speed_cmd == 'speed-neutral') {
+            speed = 1.0;
+        } else {
+            console.warn('Unknown speed command: ' + speed_cmd);
+            speed = 1.0;
+        }
+        console.log('Set speed to ' + speed);
+        document.querySelector('video').playbackRate = speed;
+    }
 })();
